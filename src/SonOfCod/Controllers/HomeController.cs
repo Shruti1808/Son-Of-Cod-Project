0288d5c0358f3a4e4423270bcec21c7343989eb8
Shruti Priya <shrutipriya1808@gmail.com>
2017-04-28 14:30:53 -0700

    not showing css and bootstrap

--- src/SonOfCod/Controllers/HomeController.cs
+++ src/SonOfCod/Controllers/HomeController.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-
+using Microsoft.AspNetCore.Authorization;
 
 namespace SonOfCod.Controllers
 {
     public class HomeController : Controller
     {
-     
+      [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
