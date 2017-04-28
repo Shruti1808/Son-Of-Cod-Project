e45dd7e3a77e5ab3266da63b12fa4231b58f4083
Shruti Priya <shrutipriya1808@gmail.com>
2017-04-28 08:58:34 -0700

    add ApplicationDbContext and Application user.cs files using Identity

--- src/SonOfCod/Startup.cs
+++ src/SonOfCod/Startup.cs
@@ -7,19 +7,39 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using FlickrClone.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-namespace SonOfCod
+namespace FlickrClone
 {
     public class Startup
     {
-        
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            
+            services.AddEntityFramework()
+            .AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+
+            //using identity 
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
         }
 
-        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
@@ -29,13 +49,16 @@ namespace SonOfCod
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseIdentity();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Account}/{action=Index}/{id?}");
             });
 
+            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
